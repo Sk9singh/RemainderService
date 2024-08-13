d0a07093759eef7d846eeff4cd3afd54743e55a0
Saksham Singh <sakshamsingh8278@gmail.com>
2024-08-13 23:58:20 +0530

    Added notification cron

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const bodyParser = require("body-parser");
 
 //const {sendBasicEmail} = require('./services/email-service');
 
-const cron = require('node-cron');
-
 const {PORT} = require('./config/serverConfig');
+
+const TicketController = require('./controllers/ticket-controller');
+
+const jobs = require('./utils/job');
 
 const setupAndStartServer = async()=> {
       const app = express();
@@ -13,20 +15,18 @@ const setupAndStartServer = async()=> {
       app.use(bodyParser.json());
       app.use(bodyParser.urlencoded({extended:true}));
 
+      app.post('/api/v1/tickets',TicketController.create);
+
 
       app.listen(PORT,()=> {
         console.log(`Server started at ${PORT}`);
-
+        jobs();
       //  sendBasicEmail(
       //    'support@admin.com',
       //    'sksingh8278@gmail.com',
       //    'This is a testing email',
       //    'Hey, how are you I hope you like the support'
       //  );
-
-      cron.schedule('*/2 * * * *',() => {
-        console.log('Running a task every two minutes');
-      });
       });
 }
 
