d5f3af703a7d7dd727ea1aa5dbd6735b5e1706bb
Saksham Singh <sakshamsingh8278@gmail.com>
2024-08-20 00:19:55 +0530

    Complete reminder service

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-//const {sendBasicEmail} = require('./services/email-service');
-
 const {PORT} = require('./config/serverConfig');
 
 const TicketController = require('./controllers/ticket-controller');
+const EmailService = require('./services/email-service');
 
 const jobs = require('./utils/job');
+
+const {subscribeMessage,createChannel} = require('./utils/messageQueue');
+const {REMINDER_BINDING_KEY} = require('./config/serverConfig');
 
 const setupAndStartServer = async()=> {
       const app = express();
@@ -17,16 +19,13 @@ const setupAndStartServer = async()=> {
 
       app.post('/api/v1/tickets',TicketController.create);
 
+      const channel = await createChannel();
+      subscribeMessage(channel,EmailService.subscribeEvents,REMINDER_BINDING_KEY);
+
 
       app.listen(PORT,()=> {
         console.log(`Server started at ${PORT}`);
-        jobs();
-      //  sendBasicEmail(
-      //    'support@admin.com',
-      //    'sksingh8278@gmail.com',
-      //    'This is a testing email',
-      //    'Hey, how are you I hope you like the support'
-      //  );
+      //  jobs();
       });
 }
 
